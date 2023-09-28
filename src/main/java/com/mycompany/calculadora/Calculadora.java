ff82856cadf9c8dd39bd0f453e23a9dce5a0b950
Eduardo F. Deola <eduardo.deola@duque.g12.br>
2023-09-28 18:46:59 -0300

    TEste

---
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadora;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author 08057
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+        FrmCalculadora calc = new FrmCalculadora();
+        calc.setTitle("Calculadora do Eduardo");
+        calc.setLocationRelativeTo(calc);
+        calc.setVisible(true);
+    }
+}
